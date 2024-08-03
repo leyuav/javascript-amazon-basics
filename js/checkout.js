7057bd888f15e40a61f63c1e9b76b920a42f783c
leyuav <leyu.av@gmail.com>
2024-08-03 11:56:40 +0200

    fixed delivery options

--- js/checkout.js
+++ js/checkout.js
@@ -37,9 +37,8 @@ cart.forEach((cartItem) => {
           <div class="delivery-option">
             <input
               type="radio"
-              checked
               class="delivery-option-input"
-              name="delivery-option-1"
+              name="delivery-option-${matchingProduct.id}"
             />
             <div>
               <div class="delivery-option-date">Tuesday, June 21</div>
@@ -47,14 +46,18 @@ cart.forEach((cartItem) => {
             </div>
           </div>
           <div class="delivery-option">
-            <input type="radio" class="delivery-option-input" name="delivery-option-1" />
+            <input type="radio" class="delivery-option-input" name="delivery-option-${
+              matchingProduct.id
+            }" />
             <div>
               <div class="delivery-option-date">Wednesday, June 15</div>
               <div class="delivery-option-price">$4.99 - Shipping</div>
             </div>
           </div>
           <div class="delivery-option">
-            <input type="radio" class="delivery-option-input" name="delivery-option-1" />
+            <input type="radio" class="delivery-option-input" name="delivery-option-${
+              matchingProduct.id
+            }" />
             <div>
               <div class="delivery-option-date">Monday, June 13</div>
               <div class="delivery-option-price">$9.99 - Shipping</div>
