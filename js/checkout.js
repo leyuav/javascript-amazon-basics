d65fd63f3de320756ac08a6d5828ac90c499354e
leyuav <leyu.av@gmail.com>
2024-08-16 19:53:52 +0200

    added interactive delivery options

--- js/checkout.js
+++ js/checkout.js
@@ -1,4 +1,10 @@
-import { cart, removeFromCart, calculateCartQuantity, updateQuantity } from '../data/cart.js';
+import {
+  cart,
+  removeFromCart,
+  calculateCartQuantity,
+  updateQuantity,
+  updateDeliveryOption,
+} from '../data/cart.js';
 import { products } from '../data/products.js';
 import { formatCurrency } from './utils/money.js';
 import dayjs from 'https://unpkg.com/dayjs@1.11.10/esm/index.js';
@@ -91,7 +97,10 @@ function deliveryOptionsHTML(matchingProduct, cartItem) {
     const isChecked = deliveryOption.id === cartItem.deliveryOptionId;
 
     html += `
-    <div class="delivery-option">
+    <div 
+    class="delivery-option js-delivery-option" 
+    data-product-id="${matchingProduct.id}"
+    data-delivery-option-id="${deliveryOption.id}">
       <input
         ${isChecked ? 'checked' : ''}
         type="radio" 
@@ -152,5 +161,12 @@ document.querySelectorAll('.js-save-link').forEach((link) => {
 
     calculateCartQuantity();
     checkoutReload();
+  });
+});
+
+document.querySelectorAll('.js-delivery-option').forEach((element) => {
+  element.addEventListener('click', () => {
+    const { productId, deliveryOptionId } = element.dataset;
+    updateDeliveryOption(productId, deliveryOptionId);
   });
 });
