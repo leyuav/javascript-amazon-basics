5883b4c9d3a099667ce0be3d6e5fc53ffcb249a6
leyuav <leyu.av@gmail.com>
2024-08-21 22:33:23 +0200

    fixed logic for item quantity update in cart
    - doesnt make sense inputting 0 when we have delete button

--- js/checkout/orderSummary.js
+++ js/checkout/orderSummary.js
@@ -116,8 +116,7 @@ export function renderOrderSummary() {
       const productId = link.dataset.productId;
       removeFromCart(productId);
 
-      const container = document.querySelector(`.js-cart-item-container-${productId}`);
-      container.remove();
+      renderOrderSummary();
 
       calculateCartQuantity();
       checkoutReload();
@@ -141,8 +140,8 @@ export function renderOrderSummary() {
 
       const newQuantity = Number(quantityInput.value);
 
-      if (newQuantity < 0 || newQuantity >= 1000) {
-        alert('Quantity must be at least 0 and less than 1000');
+      if (newQuantity <= 0 || newQuantity >= 1000) {
+        alert('Quantity must be at least 1 and less than 1000');
         return;
       }
 
